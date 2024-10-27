9fbbcce369c163e3b777c7a6569514fe7b3593ca
Halwaaaa <halwadev@gmail.com>
2024-10-27 00:19:43 -0700

    add GetIt

--- lib/core/utles/Get_it.dart
+++ lib/core/utles/Get_it.dart
@@ -1,11 +1,23 @@
 import 'package:bookdoctor/featuers/Auth/Data/RemotleDataSource/SingRemote.dart';
 import 'package:bookdoctor/featuers/Auth/Data/Repoes/SingRepo.dart';
+import 'package:bookdoctor/featuers/Auth/domin/Repos/SingRepo.dart';
+import 'package:bookdoctor/featuers/Auth/domin/UseCase/AskToSingFeaTuredUseCase.dart';
 import 'package:get_it/get_it.dart';
 
-class Get_it {
-  static final getIt = GetIt.instance;
-  // static void g() {
-  //   getIt.registerLazySingleton<SingReposImplo>(() =>
-  //       SingReposImplo(singRemoteDataSousrce: SingRemoteDataSousrceImp()));
-  // }
+class get_it {
+  final getIt = GetIt.instance;
+
+  void setup() {
+    // تسجيل SingRemoteDataSourceImp كمصدر للبيانات
+    getIt.registerCachedFactory<SingRemoteDataSousrce>(
+        () => SingRemoteDataSousrceImp());
+
+    // تسجيل SingReposImplo كمستودع
+    getIt.registerCachedFactory<SingRepo>(() =>
+        SingReposImplo(singRemoteDataSousrce: getIt<SingRemoteDataSousrce>()));
+
+    // تسجيل AskToSingFeaTuredUseCase كحالة استخدام
+    getIt.registerCachedFactory<AskToSingFeaTuredUseCase>(
+        () => AskToSingFeaTuredUseCase(singRepo: getIt<SingRepo>()));
+  }
 }
